23b9b646576a5b024ca6294296d2f171646c6e23
jaceksen <jaceksen@gmail.com>
2017-09-22 17:18:00 +0200

    content provider

---
+++ app/src/main/java/pl/jaceksen/contentprovider/ContactItems.java
@@ -0,0 +1,17 @@
+package pl.jaceksen.contentprovider;
+
+/**
+ * Created by jsen on 22.09.17.
+ */
+
+public class ContactItems {
+
+    String name;
+    String phoneNumber;
+
+    public ContactItems(String name, String phoneNumber){
+        this.name=name;
+        this.phoneNumber=phoneNumber;
+    }
+
+}
